48f54bdc78bd841f44f94c9b1cfa7b7a4c3a5ea2
chaosannals <chaosannals@outlook.com>
2022-06-29 17:37:42 +0800

    save

--- captcha_infer.py
+++ captcha_infer.py
@@ -4,21 +4,21 @@ from PIL import Image
 from exert.captcha.model import CaptchaNet
 from exert.captcha.dataset import make_transform, answer_v2s
 
-def infer(path):
+def infer(m, path):
     t = make_transform()
     i = Image.open(path)
     r = t(i.convert('RGB')).view(1, 3, 128, 64)
-    m = CaptchaNet()
-    m.load('./captcha.m')
     a = m.answer(r)
     return answer_v2s(a[0])
 
 if __name__ == '__main__':
     ps = glob.glob('./assets/captchas/*.png')
     pmax = len(ps) - 1
+    m = CaptchaNet()
+    m.load('./captcha.m')
     for i in range(10):
         k = random.randint(0, pmax)
         p = ps[k]
         print(p)
-        a = infer(p)
+        a = infer(m, p)
         print(a)
